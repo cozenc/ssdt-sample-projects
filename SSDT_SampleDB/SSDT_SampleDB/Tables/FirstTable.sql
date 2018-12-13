4d02e80206c596d990a72409938878b8d3a9c974
caglarozenc <caglarozenc@gmail.com>
2018-12-13 10:10:58 +0300

    FirsTable Create

---
+++ SSDT_SampleDB/SSDT_SampleDB/Tables/FirstTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[FirstTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NULL, 
+    [Surname] VARCHAR(50) NULL, 
+    [CreatedDate] DATETIME NOT NULL DEFAULT getdate()
+)
+
